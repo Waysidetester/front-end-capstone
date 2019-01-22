e7b0b163e42db81a5e55f4d6eab0171136a457f2
WaysideTester <dylanmurray211@outlook.com>
2019-01-21 19:59:49 -0600

    conditional navbar based on authed status

--- src/components/MyNav/MyNav.js
+++ src/components/MyNav/MyNav.js
@@ -6,14 +6,34 @@ import {
   NavItem,
   NavLink,
 } from 'reactstrap';
+import PropTypes from 'prop-types';
 import fbMethods from '../../helpers/firebase/fbMethods';
 
 class MyNav extends React.Component {
+  static propTypes = {
+    authed: PropTypes.bool,
+  }
+
   render() {
+    if (this.props.authed) {
+      return (
+        <div>
+          <Navbar color="dark" dark expand="md">
+            <NavbarBrand href="/">Renenutet</NavbarBrand>
+              <Nav className="ml-auto" navbar>
+                <NavItem onClick={fbMethods.logout}>
+                  <NavLink >Sign Out</NavLink>
+                </NavItem>
+              </Nav>
+          </Navbar>
+        </div>
+      );
+    }
+
     return (
       <div>
         <Navbar color="dark" dark expand="md">
-          <NavbarBrand href="/">reactstrap</NavbarBrand>
+          <NavbarBrand href="/">Renenutet</NavbarBrand>
             <Nav className="ml-auto" navbar>
               <NavItem onClick={fbMethods.googleLogin}>
                 <NavLink >Sign In</NavLink>
